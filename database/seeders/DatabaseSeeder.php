357b8432eca58a1e5bf6d4d4b0f34054968d9e59
kmmaniez <dzlabsid@gmail.com>
2023-08-20 18:16:16 +0700

    update db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Infrastruktur;
 use App\Models\Role;
 use App\Models\Ruangan;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // INI BUAT GENERATE NAMA RUANGAN
         $alphabet = ['A','B','C','D','E'];
         for ($i=0; $i < count($alphabet); $i++) { 
             Ruangan::create([
@@ -26,37 +28,44 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
+        // INI BUAT GENERATE NAMA ROLE (ADMIN & PENGGUNA)
         for ($i=0; $i < 2; $i++) { 
             Role::create([
                 'nama_role' => $i%2==0 ? 'admin' : 'pengguna'
             ]);
         }
 
+        // INI BUAT GENERATE ADMIN & PENGGUNA
         for ($i=0; $i < 2; $i++) { 
             User::create([
                 'id_role' => $i%2==0 ? 1 : 2,
-                'name' => $i%2==0 ? 'Admin User' : 'Pengguna User',
+                'nama' => $i%2==0 ? 'Admin User' : 'Pengguna User',
+                'username' => $i%2==0 ? 'iniadmin' : 'inipengguna',
                 'email' => $i%2==0 ? 'admin@gmail.com' : 'pengguna@gmail.com',
                 'password' => Hash::make('password')
             ]);
         }
 
-        for ($i=0; $i < 10; $i++) { 
+        // INI BUAT GENERATE PENGGUNA (8 ORG)
+        for ($i=0; $i < 8; $i++) { 
             User::create([
                 'id_role' => 2,
-                'name' => fake('id_ID')->name(),
+                'nama' => fake('id_ID')->name(),
+                'username' => fake('id_ID')->userName(),
                 'email' => fake('id_ID')->email(),
                 'password' => Hash::make('password')
             ]);
         }
 
-        $barang = ['Kamera','Laptop','Tripod','Komputer','Dokumen'];
+        // INI BUAT GENERATE INFRASTRUKTUR (10 BRG RANDOM, RANDOM PENGGUNA, RANDOM RUANG)
+        $barang = ['Kamera','Laptop','Tripod','Komputer','Dokumen','Lightning','Map'];
         for ($i=0; $i < 10; $i++) { 
             Infrastruktur::create([
-                'id_user' => rand(3,12),
+                'id_user' => rand(3,10),
                 'id_ruang' => rand(1,5),
-                'nama_barang' => 'Barang '.$barang[rand(0,4)],
-                'tanggal_dipakai' => fake()->date()
+                'nama_barang' => $barang[rand(0,6)],
+                'tgl_mulai' => fake()->date(),
+                'tgl_selesai' => fake()->date()
             ]);
         }
         
