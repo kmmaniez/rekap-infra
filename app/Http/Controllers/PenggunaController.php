88bcb1eeb99613dd74bae60c6f6bc589d9ba3014
kmmaniez <dzlabsid@gmail.com>
2023-08-29 21:25:38 +0700

    update controller pengguna

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ruangan;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PenggunaController extends Controller
 {
     public function index()
     {
         return view('admin.pengguna.index',[
-            'pengguna' => User::with('infrastrukturs')->whereHas('infrastrukturs')->get()
+            // 'pengguna' => User::with('infrastrukturs')->whereHas('infrastrukturs')->get()
+            'pengguna' => User::all()
         ]);
     }
 
@@ -19,6 +22,42 @@ class PenggunaController extends Controller
      */
     public function create()
     {
-        return view('admin.ruangan.create');
+        return view('admin.pengguna.create');
+    }
+    
+    public function store(Request $request)
+    {
+        User::create([
+            'id_role' => $request->role,
+            'nama' => $request->nama_pengguna,
+            'username' => $request->username_pengguna,
+            'password' => Hash::make($request->password_pengguna)
+        ]);
+        return redirect()->to(route('pengguna.index'));
+    }
+
+    public function edit(User $pengguna)
+    {
+        return view('admin.pengguna.edit',[
+            'pengguna' => $pengguna
+        ]);
+    }
+
+    public function update(Request $request, User $pengguna)
+    {
+        $pengguna->update([
+            'id_role' => $request->role,
+            'nama' => $request->nama_pengguna,
+            'username' => $request->username_pengguna,
+            // 'password' => Hash::make($request->password_pengguna)
+        ]);
+        return redirect()->to(route('pengguna.index'));
+
+    }
+
+    public function destroy(User $pengguna)
+    {
+        $pengguna->delete();
+        return redirect()->to(route('pengguna.index'));
     }
 }
